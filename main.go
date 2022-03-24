8204b942707372f4342601758966c99686845fc3
Ellie Bahadori <elliedori@gmail.com>
2022-03-24 11:29:24 -0700

    Add booking logic based on number of tickets requested

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "strings"
 
 func main() {
 	conferenceName := "Freedom Conference"
@@ -13,12 +14,50 @@ func main() {
 	fmt.Printf("We have a total of %v tickets, with %v still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets to attend")
 
-	var userName string
-	var userTickets int
-	// this is a comment
+	var bookings []string
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+	for {
 
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+
+		fmt.Println("Enter your first name")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email address")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter number of tickets you'd like")
+		fmt.Scan(&userTickets)
+
+		if userTickets < remainingTickets {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
+
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+
+			fmt.Printf("%v tickets remaining\n", remainingTickets)
+
+			firstNames := []string{}
+
+			for _, booking := range bookings {
+				names := strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+
+			fmt.Printf("All booking first names: %v\n", firstNames)
+			if remainingTickets == 0 {
+				// end the program
+				fmt.Println("All tickets are sold out! Come back next year :)")
+				break
+			}
+		} else {
+			fmt.Printf("Sorry, we only have %v tickets available\n", remainingTickets)
+		}
+	}
 }
